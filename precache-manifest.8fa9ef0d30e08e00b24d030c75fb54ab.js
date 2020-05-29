e6b42f3b63766c6f3b7732f98da915600158bd73
Shreesh Nayak <shreeshnayak@gmail.com>
2020-05-29 14:44:44 -0700

    Updates

---
+++ precache-manifest.8fa9ef0d30e08e00b24d030c75fb54ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e53598c08c8dc68a9823be36cd3b1944",
+    "url": "/myportfoliosite/index.html"
+  },
+  {
+    "revision": "43b03f39a6804a38f436",
+    "url": "/myportfoliosite/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "29bd143ebe3fcd384772",
+    "url": "/myportfoliosite/static/css/main.ca44b6af.chunk.css"
+  },
+  {
+    "revision": "43b03f39a6804a38f436",
+    "url": "/myportfoliosite/static/js/2.512a8bd3.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/myportfoliosite/static/js/2.512a8bd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29bd143ebe3fcd384772",
+    "url": "/myportfoliosite/static/js/main.7157fde7.chunk.js"
+  },
+  {
+    "revision": "d24862d4f8ceb65b8a3a",
+    "url": "/myportfoliosite/static/js/runtime-main.ca71ad0e.js"
+  },
+  {
+    "revision": "e4c2ea96ad1ec2661aef6b390dc33d81",
+    "url": "/myportfoliosite/static/media/avatar.e4c2ea96.png"
+  }
+]);
