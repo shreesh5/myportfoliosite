b89c6dfe7b3659d6682aa81c61849882a47fb21c
Shreesh Nayak <shreeshnayak@gmail.com>
2020-05-29 16:00:14 -0700

    updated paths8

--- src/App.js
+++ src/App.js
@@ -26,9 +26,9 @@ class App extends React.Component {
              </Header>
              <Drawer title="Title">
                  <Navigation>
-                     <Link to="/myportfoliosite/aboutme/">About Me</Link>
-                     <Link to="/myportfoliosite/projects/">Projects</Link>
-                     <Link to="/myportfoliosite/contact/">Contact</Link>
+                     <Link to="/myportfoliosite/aboutme">About Me</Link>
+                     <Link to="/myportfoliosite/projects">Projects</Link>
+                     <Link to="/myportfoliosite/contact">Contact</Link>
                  </Navigation>
              </Drawer>
              <Content>
