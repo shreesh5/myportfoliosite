70c04965df5809ca3e798f7289f0d932b1ce25e9
Shreesh Nayak <shreeshnayak@gmail.com>
2020-05-29 15:38:15 -0700

    Updates

--- precache-manifest.c95fbd0279e00bae5c25888b162bbff2.js
+++ precache-manifest.c95fbd0279e00bae5c25888b162bbff2.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "806969082b064a3beb867e84010cf0f1",
+    "revision": "e35d5e3059d4538601a9d6922ee46fd0",
     "url": "/myportfoliosite/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/myportfoliosite/static/css/2.b6a8c48d.chunk.css"
   },
   {
-    "revision": "ba204dc7cea877b9f4d1",
+    "revision": "2e1517bd6199c47aefc1",
     "url": "/myportfoliosite/static/css/main.ca44b6af.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/myportfoliosite/static/js/2.512a8bd3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ba204dc7cea877b9f4d1",
-    "url": "/myportfoliosite/static/js/main.dac7e2ff.chunk.js"
+    "revision": "2e1517bd6199c47aefc1",
+    "url": "/myportfoliosite/static/js/main.1184bd2e.chunk.js"
   },
   {
     "revision": "d24862d4f8ceb65b8a3a",
