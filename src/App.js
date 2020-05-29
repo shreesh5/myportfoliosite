8817cc3bb476e38ee919ff448fc913ddf7f60c42
Shreesh Nayak <shreeshnayak@gmail.com>
2020-05-29 15:58:21 -0700

    updated paths7

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ class App extends React.Component {
          <Layout>
              <Header className="header-color" title="My Portfolio" scroll>
                  <Navigation>
-                     <Link to="/aboutme/">About Me</Link>
-                     <Link to="/projects/">Projects</Link>
-                     <Link to="/contact/">Contact</Link>
+                     <Link to="/aboutme">About Me</Link>
+                     <Link to="/projects">Projects</Link>
+                     <Link to="/contact">Contact</Link>
                  </Navigation>
              </Header>
              <Drawer title="Title">
