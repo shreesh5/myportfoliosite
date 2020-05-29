263d12075b492273e2b5eaffe0f1053941afd500
Shreesh Nayak <shreeshnayak@gmail.com>
2020-05-29 15:27:24 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myportfoliosite/precache-manifest.b23547e94ffe33418204449cb89f5f1b.js"
+  "/myportfoliosite/precache-manifest.fc10c4284c2841a96782ac9a7849ef95.js"
 );
 
 self.addEventListener('message', (event) => {
